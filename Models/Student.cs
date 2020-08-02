7738b13e25e7766e909acd91b38f38b97764e236
marqzosvaldo <crosser.xr@gmail.com>
2020-08-01 23:27:04 -0500

    Entities

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models {
+    public class Student {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
