6de0cec320639b85f285d812bbc70df4149e8f85
marqzosvaldo <crosser.xr@gmail.com>
2020-08-02 01:11:42 -0500

    string to int model  attribute fixed

--- Models/Course.cs
+++ Models/Course.cs
@@ -6,7 +6,7 @@ namespace ContosoUniversity.Models {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
         public string Title { get; set; }
-        public string Credits { get; set; }
+        public int Credits { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
 
